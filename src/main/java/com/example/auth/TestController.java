cab1b6edc10ea8e3f116910bf312c32bf99cab06
Szymon <szymon.osinski.2009@gmail.com>
2026-06-25 16:40:31 +0200

    chore: TestController that check connection with nlp-service

--- src/main/java/com/example/auth/TestController.java
+++ src/main/java/com/example/auth/TestController.java
@@ -1,12 +1,21 @@
 package com.example.auth;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@AllArgsConstructor
 public class TestController {
+
+  private final RestTemplate restTemplate;
+
   @GetMapping("/test")
   public String helloWorld() {
-    return "Hello world";
+    return restTemplate.getForObject(
+        "http://nlp-service:8000/health",
+        String.class
+    );
   }
 }
