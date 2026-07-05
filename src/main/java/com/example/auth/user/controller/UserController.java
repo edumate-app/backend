dd2560275d48fe71a521bea2946d4dbe845ef546
Szymon <szymon.osinski.2009@gmail.com>
2026-07-05 17:14:19 +0200

    feat: change nativeLang endpoint PATCH /native-lang; add PATCH method to CORS config

--- src/main/java/com/example/auth/user/controller/UserController.java
+++ src/main/java/com/example/auth/user/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.example.auth.user.controller;
 
 import com.example.auth.auth.dto.UserDto;
+import com.example.auth.user.dto.UpdateNativeLangRequest;
 import com.example.auth.user.entity.AppUser;
 import com.example.auth.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -19,5 +18,10 @@ public class UserController {
   @GetMapping("/me")
   public UserDto me(@AuthenticationPrincipal AppUser user) {
     return userService.me(user);
+  }
+
+  @PatchMapping("/native-lang")
+  public void updateNativeLang(@AuthenticationPrincipal AppUser user, @RequestBody UpdateNativeLangRequest request) {
+    userService.updateNativeLang(user, request.lang());
   }
 }
--- src/main/java/com/example/auth/user/service/UserService.java
+++ src/main/java/com/example/auth/user/service/UserService.java
@@ -2,16 +2,26 @@ package com.example.auth.user.service;
 
 import com.example.auth.auth.dto.UserDto;
 import com.example.auth.user.entity.AppUser;
+import com.example.auth.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class UserService {
+  private final UserRepository userRepository;
   public UserDto me(AppUser user) {
     return new UserDto(
         user.getName(),
         user.getEmail(),
         user.getAvatarUrl(),
+        user.getNativeLang(),
         user.getProvider()
     );
+  }
+
+  public void updateNativeLang(AppUser user, String lang) {
+    user.setNativeLang(lang);
+    userRepository.save(user);
   }
 }
