e09e5cc95dc7784cda54fe1345980ac23abb47cf
Szymon <szymon.osinski.2009@gmail.com>
2026-07-28 14:12:24 +0200

    fix(WordAnalyzedDto): add conjugation

--- src/main/java/com/example/auth/expression/dto/WordAnalyzedDto.java
+++ src/main/java/com/example/auth/expression/dto/WordAnalyzedDto.java
@@ -1,12 +1,16 @@
 package com.example.auth.expression.dto;
 
+import java.util.List;
+
 public record WordAnalyzedDto(
     String text,
     String lemma,
     PosType pos,
     NumberType number,
+    Integer person,
     String tense,
     String mood,
-    String gender
+    String gender,
+    List<VerbConjugationForm> conjugation
 ) {
 }
