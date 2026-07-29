93762c92301d2d92e2827befd22d83cb9bd6bd90
Szymon <szymon.osinski.2009@gmail.com>
2026-07-29 16:00:10 +0200

    feat: MinioConfig, StorageProperties

---
+++ src/main/java/com/example/auth/config/StorageProperties.java
@@ -0,0 +1,12 @@
+package com.example.auth.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "storage")
+public record StorageProperties(
+    String endpoint,
+    String accessKey,
+    String secretKey,
+    String bucket
+) {
+}
